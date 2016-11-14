84ac977a616d4adf9d0e10fae810ac3dfc546146
Martin Paoletta <martin@tranzeal.com>
2016-11-14 17:40:31 -0300

    benchmark

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -4,17 +4,24 @@ import scala.sys.process._
 import java.io.File
 
 val reportName = args(0)
-val 
 
 val gatlingPath = sys.env.get("GATLING_HOME").getOrElse(throw new Exception("No esta definida GATLING_HOME")) + "/bin/gatling.sh"
 
 val cmd = s"$gatlingPath -s io.redbee.proxytest.ProxyTest -m -rf /tmp/proxy2"
 
-val result = cmd !!
+var buffer = new StringBuffer()
+val processLogger = ProcessLogger(line => {
+	println(line)
+	buffer.append(line).append("\n")
+}, line => {
+	println("ERROR: " + line)
+})
+
+cmd ! processLogger
 
 val tag = "Please open the following file:"
 
-result.split("\n").find(_.startsWith(tag)) match {
+buffer.toString.split("\n").find(_.startsWith(tag)) match {
 
 	case Some(line) => {
 		val folder = new File(line.drop(tag.size)).getParentFile
@@ -22,7 +29,7 @@ result.split("\n").find(_.startsWith(tag)) match {
 		val mv = s"mv $folder $dst"
 		mv !
 	}
-	case None => println("No se genero salida. Revisar output:\n" + result)
+	case None => println("No se genero salida. Revisar output:\n" + buffer)
 }
 
 
